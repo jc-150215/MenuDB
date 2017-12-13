63cca5ff55d30635706e46ead22f834d0a9daa1a
仁一 伊藤 <150215@jc-21.jp>
2017-12-13 14:53:48 +0900

    タブ追加

--- Menu/Menu/Menu.xaml.cs
+++ Menu/Menu/Menu.xaml.cs
@@ -10,25 +10,59 @@ using Xamarin.Forms.Xaml;
 namespace Menu
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class Menu : ContentPage
+    public partial class Menu : TabbedPage
     {
         public Menu()
         {
             InitializeComponent();
 
-            // iOSだけ、上部に余白をとる
-            this.Padding = new Thickness(0, Device.OnPlatform(20, 0, 0), 0, 0);
 
-            String[] array1 = new String[] { bbbbb, ccccc, ggggg, eeeee, aaaaa };
+            int[] array1 = new int[] { 1, 2, 3, 4, 5 };
 
             // ListViewを生成する
             this.listView.ItemsSource = array1;
 
+
+            this.Padding = new Thickness
+            {
+                //iOSの場合にのみ画面上部に20ピクセルのスペースを空ける
+                Top = Device.OnPlatform(20, 0, 0)
+            };
+
+            //Childrenプロパティにページをそのまま追加
+            this.Children.Add(
+                new ContentPage
+                {
+                    Title = "Red",
+                    BackgroundColor = Color.Red,
+                }
+            );
+
+            //ContentPageクラスのインスタンスを生成
+            var tab2 = new ContentPage
+            {
+                Title = "Blue",
+                BackgroundColor = Color.Blue,
+            };
+
+            //ContentPageクラス内でコントロールを作成
+            var tab3 = new ContentPage
+            {
+                Title = "Green",
+                Content = new BoxView
+                {
+                    BackgroundColor = Color.Green,
+                },
+            };
+
+            //tab2, tab3をChildrenプロパティに追加
+            this.Children.Add(tab2);
+            this.Children.Add(tab3);
         }
 
         private void Select_SearchButtonPressed(object sender, EventArgs e)
         {
-            DisplayAlert("", SearchCommand, "OK");
+            //DisplayAlert("", SearchCommand, "OK");
         }
     }
 }

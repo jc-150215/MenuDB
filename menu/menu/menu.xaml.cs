295701cce6b72ade85ca0d6114e029e451d780f2
仁一 伊藤 <150215@jc-21.jp>
2017-12-13 16:19:24 +0900

    button 追加

--- menu/menu/menu.xaml.cs
+++ menu/menu/menu.xaml.cs
@@ -10,57 +10,40 @@ using Xamarin.Forms.Xaml;
 namespace Menu
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class Menu : TabbedPage
+    public partial class Menu : ContentPage
     {
         public Menu()
         {
-            //InitializeComponent();
+            InitializeComponent();
 
-            //int[] array1 = new int[] { 1, 2, 3, 4, 5 };
+            // iOSだけ、上部に余白をとる
+            this.Padding = new Thickness(0, Device.OnPlatform(20, 0, 0), 0, 0);
+
+            int[] array1 = new int[] { 2, 3, 1, 5, 6 };
 
             // ListViewを生成する
-            //this.listView.ItemsSource = array1;
-
-            this.Padding = new Thickness
-            {
-                //iOSの場合にのみ画面上部に20ピクセルのスペースを空ける
-                Top = Device.OnPlatform(20, 0, 0)
-            };
-
-            //Childrenプロパティにページをそのまま追加
-            var tab1 = new ContentPage
-            {
-                Title = "Red",
-                BackgroundColor = Color.Red,
-            };
-
-            //ContentPageクラスのインスタンスを生成
-            var tab2 = new ContentPage
-            {
-                Title = "Blue",
-                BackgroundColor = Color.Blue,
-            };
-
-            //ContentPageクラス内でコントロールを作成
-            var tab3 = new ContentPage
-            {
-                Title = "Green",
-                Content = new BoxView
-                {
-                    BackgroundColor = Color.Green,
-                },
-            };
-
-            //tab2, tab3をChildrenプロパティに追加
-            this.Children.Add(tab1);
-            this.Children.Add(tab2);
-            this.Children.Add(tab3);
-
+            this.listView.ItemsSource = array1;
         }
 
         private void Select_SearchButtonPressed(object sender, EventArgs e)
         {
             //DisplayAlert("", SearchCommand, "OK");
+        }
+
+        private void 昇順_Clicked(object sender, EventArgs e)
+        {
+            int[] array1 = new int[] { 1, 2, 3, 4, 5 };
+
+            // ListViewを生成する
+            this.listView.ItemsSource = array1;
+        }
+
+        private void 降順_Clicked(object sender, EventArgs e)
+        {
+            int[] array1 = new int[] { 5, 4, 3, 2, 1 };
+
+            // ListViewを生成する
+            this.listView.ItemsSource = array1;
         }
     }
 }

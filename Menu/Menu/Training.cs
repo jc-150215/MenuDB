c2b9e5778e85e7f764d453b5a4e83ccefd03df50
仁一 伊藤 <150215@jc-21.jp>
2017-12-18 16:39:50 +0900

    ArrayList 変更

--- Menu/Menu/Training.cs
+++ Menu/Menu/Training.cs
@@ -8,23 +8,21 @@ namespace Menu
 {
     class Training
     {
-        private String menu;   //タイトル
-        private String load;    //ISBNコード
+        private String menu;   
+        private String load;    
 
-        //コンストラクタ
+
         public Training(String menu, String load)
         {
             this.menu = menu;
             this.load = load;
         }
-
-        //タイトルのゲッター
+        
         public String getMenu()
         {
             return menu;
         }
-
-        //ISBNコードのゲッター
+        
         public String getLoad()
         {
             return load;
